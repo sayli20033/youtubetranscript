6daaa9257f6849e0ab214c156ee47cc1f99f67ea
sayli20033 <151854358+sayli20033@users.noreply.github.com>
2024-09-16 23:26:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,4 +33,18 @@ with st.sidebar:
 st.feedback("stars")
 
 st.balloons()
+
+pages = {
+    "Your account": [
+        st.Page("create_account.py", title="Create your account"),
+        st.Page("manage_account.py", title="Manage your account"),
+    ],
+    "Resources": [
+        st.Page("learn.py", title="Learn about us"),
+        st.Page("trial.py", title="Try it out"),
+    ],
+}
+
+pg = st.navigation(pages)
+pg.run()
     
