77e32268c9a9df51435b83048b0c80388baca21c
sayli20033 <151854358+sayli20033@users.noreply.github.com>
2024-09-16 17:36:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title("PPAS SUMMARY :- YOUTUBE VIDEO SUMMARIZER")
 #subheader
 st.subheader("get YouTube transcript and use AI to summerize YouTube videos in one click for free online with PPAS YouTube Summary tool")
 st.subheader("want to summarize YouTube videos??")
-st.markdown("Enter YouTube Video Links:")
+youtube_link = st.text_input("Enter YouTube Video Link:")
 
 st.button("Get Detailed Notes")
 if youtube_link:
