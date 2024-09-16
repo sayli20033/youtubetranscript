8817c5ae43fe0532bbad4b8f5e183220cd280c75
sayli20033 <151854358+sayli20033@users.noreply.github.com>
2024-09-17 00:16:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,50 +1,4 @@
 import streamlit as st
-from dotenv import load_dotenv
-# load_dotenv() ##load all the nevironment variables
-import os
-import google.generativeai as genai
-
-from youtube_transcript_api import YouTubeTranscriptApi
-
-# transcript_list = YouTubeTranscriptApi.list_transcripts('Y9Um-8nPnVQ')
-# for transcript in transcript_list:
-#      print(transcript.fetch())
-#      german = transcript.translate('mr').fetch()
-#      print(transcript.translate('mr').fetch())
-
-# new things added above it 
-# genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
-prompt="""You are Yotube video summarizer. You will be taking the transcript text
-and summarizing the entire video and providing the important summary in points
-within 250 words. Please provide the summary of the text given here:  """
-
-
-
-## getting the transcript data from yt videos
-def extract_transcript_details(youtube_video_url):
-    try:
-        video_id=youtube_video_url.split("=")[1]
-        
-        transcript_text=YouTubeTranscriptApi.get_transcript(video_id)
-
-        transcript = ""
-        for i in transcript_text:
-            transcript += " " + i["text"]
-
-        return transcript
-
-    except Exception as e:
-        raise e
-
-    
-    
-## getting the summary based on Prompt from Google Gemini Pro
-def generate_gemini_content(transcript_text,prompt):
-
-    model=genai.GenerativeModel("gemini-pro")
-    response=model.generate_content(prompt+transcript_text)
-    return response.text
 
 
 
@@ -62,32 +16,12 @@ if youtube_link:
 
 if st.button("Get Detailed Notes"):
     transcript_text=extract_transcript_details(youtube_link)
-    
-    if transcript_text:
-        summary=generate_gemini_content(transcript_text,prompt)
-        st.markdown("## Detailed Notes:")
-        st.write(transcript_text)
-        # st.markdown("## Detailed Notes in Marathi :")
-        # st.write(german)
-        # Fetch the Marathi translation (YouTube Transcript API)
-        transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
-        german = None
-        for transcript in transcript_list:
-                german = transcript.translate('mr').fetch()
-
-        if german:
-            # Concatenate the Marathi transcript into a single paragraph
-            marathi_text = " ".join([line['text'] for line in german])
-            st.markdown("## Detailed Notes in Marathi:")
-            st.write(marathi_text)
-        else:
-            st.write("Marathi translation not available for this video.")
 
 
 
 st.subheader("Why Use YouTube Video Summerizer?")
 st.markdown("    1) Fast And Accurate")
-st.markdown("        2) Free Of charge")
+st.markdown("    2) Free Of charge")
 st.markdown("    3) Multilanguage Support")
 
 st.subheader("How It Works")
