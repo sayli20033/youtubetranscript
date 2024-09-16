7614ca0064eb46e011590ddaddb41c65c6b5f8a0
sayli20033 <151854358+sayli20033@users.noreply.github.com>
2024-09-16 22:57:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,8 @@ st.write("3) Copy and Use: With just a click, the transcript will be copied to y
 with st.sidebar:
     st.header("features")
     with st.form("my_form"):
-        st.write("Inside the form")
-        st.text_input("text input")
-        st.text_area("text area")
+        #st.write("Inside the form")
+       # st.text_input("text input")
+        #st.text_area("text area")
         st.button("submit form")
     
