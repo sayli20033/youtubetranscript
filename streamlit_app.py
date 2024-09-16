040b63ed3ed992d3b308850ca7ffc7be8dc70447
sayli20033 <151854358+sayli20033@users.noreply.github.com>
2024-09-17 00:28:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,50 @@
 import streamlit as st
+from dotenv import load_dotenv
+
+# load_dotenv() ##load all the nevironment variables
+import os
+import google.generativeai as genai
+
+from youtube_transcript_api import YouTubeTranscriptApi
+
+# transcript_list = YouTubeTranscriptApi.list_transcripts('Y9Um-8nPnVQ')
+# for transcript in transcript_list:
+#      print(transcript.fetch())
+#      german = transcript.translate('mr').fetch()
+#      print(transcript.translate('mr').fetch())
+
+# new things added above it 
+# genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+prompt="""You are Yotube video summarizer. You will be taking the transcript text
+and summarizing the entire video and providing the important summary in points
+within 250 words. Please provide the summary of the text given here:  """
 
 
 
+## getting the transcript data from yt videos
+def extract_transcript_details(youtube_video_url):
+    try:
+        video_id=youtube_video_url.split("=")[1]
+        
+        transcript_text=YouTubeTranscriptApi.get_transcript(video_id)
+
+        transcript = ""
+        for i in transcript_text:
+            transcript += " " + i["text"]
+
+        return transcript
+
+    except Exception as e:
+        raise e
 
 
+## getting the summary based on Prompt from Google Gemini Pro
+def generate_gemini_content(transcript_text,prompt):
+
+    model=genai.GenerativeModel("gemini-pro")
+    response=model.generate_content(prompt+transcript_text)
+    return response.text
 
 #title
 st.title("PPAS SUMMARY :- YOUTUBE VIDEO SUMMARIZER")
