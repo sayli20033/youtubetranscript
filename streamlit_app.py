9bac6b7ce461497529de5dbf76ba39f8ff29f000
sayli20033 <151854358+sayli20033@users.noreply.github.com>
2024-09-16 16:44:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+#title
+st.title("PPAS SUMMARY :- YOUTUBE VIDEO SUMMARIZER")
+#subheader
+st.subheader("get YouTube transcript and use AI to summerize YouTube videos in one click for free online with PPAS YouTube Summary tool")
+st.subheader("want to summarize YouTube videos??")
+st.markdown("Enter YouTube Video Links:")
 
-st.write('Hello world!')
+st.button("Get Detailed Notes")
